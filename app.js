90f78003f8a529c314f113111a2bfb1e7408d74e
Fedor Kharitonov <kharitonov.fedor@gmail.com>
2021-06-30 18:39:16 +0200

    update

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const canvas = document.getElementById('abc');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    draw();
+});
 
 const ctx = canvas.getContext('2d');
-
-ctx.beginPath();
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50); // линия вправо
-ctx.lineTo(150, 150); // линия вниз
-ctx.lineTo(50, 150); // линия влево
-ctx.closePath(); // смыкание начала и конца рисунка (левая стена)
-ctx.stroke();
-
-console.log(ctx);
+const draw = () => {
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineTo(150, 50); // линия вправо
+    ctx.lineTo(150, 150); // линия вниз
+    ctx.lineTo(50, 150); // линия влево
+    ctx.closePath(); // смыкание начала и конца рисунка (левая стена)
+    ctx.stroke();
+    console.log(ctx);
+};
