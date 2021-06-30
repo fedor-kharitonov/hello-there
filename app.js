6dbd50677feca07fe50771604cd06c94cab35c67
Fedor Kharitonov <kharitonov.fedor@gmail.com>
2021-06-30 17:35:50 +0200

    step 1

---
+++ app.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById('abc');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50); // линия вправо
+ctx.lineTo(150, 150); // линия вниз
+ctx.lineTo(50, 150); // линия влево
+ctx.closePath(); // смыкание начала и конца рисунка (левая стена)
+ctx.stroke();
